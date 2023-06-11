f0b5ce34408f981df96fc63c180d4efebb74e9e2
YuvalShaffir <34415892+YuvalShaffir@users.noreply.github.com>
2023-06-11 20:43:04 +0300

    successfully created a connection between the phone and the computer

--- main.py
+++ main.py
@@ -3,9 +3,11 @@ import cv2
 import numpy as np
 import sys
 import requests
+import imutils
+
 
 VIDEO_FEED_ERR = "Video feed not working. Exiting..."
-url = "http://localhost:5000/"
+url = "http://10.10.1.152:8080/shot.jpg"
 
 
 # def video_feed_ok():
@@ -18,14 +20,36 @@ def play_game():
 
 def main(args):
 
-    # connect to the phones camera
-    img_resp = requests.get(url)
-    img_arr = np.array(bytearray(img_resp.content), dtype=np.uint8)
-    imgOriginalScene = cv2.imdecode(img_arr, -1)
+    # connect to the phone's camera
+    # While loop to continuously fetching data from the Url
+    while True:
+        try:
+            img_resp = requests.get(url)
+            # Process the response if the connection is successful
+            print(img_resp.status_code)
+        except requests.ConnectionError:
+            # Handle connection-related errors
+            raise Exception("A connection error occurred.")
+        except requests.Timeout:
+            # Handle timeout error
+            raise Exception("The request timed out.")
+        except requests.TooManyRedirects:
+            # Handle too many redirects error
+            raise Exception("Too many redirects occurred.")
+        except requests.RequestException:
+            # Handle other request-related errors
+            raise Exception("An error occurred during the request.")
+        img_arr = np.array(bytearray(img_resp.content), dtype=np.uint8)
+        img = cv2.imdecode(img_arr, -1)
+        img = imutils.resize(img, width=1000, height=1800)
+        cv2.imshow("Phone Camera - (Press Esc to exit)", img)
 
-    cv2.imshow("IPcamera", imgOriginalScene)
-    cv2.namedWindow('IPcamera', cv2.WINDOW_NORMAL)
-    cv2.resizeWindow('IPcamera', 300, 300)
+        # Press Esc key to exit
+        if cv2.waitKey(1) == 27:
+            break
+
+    cv2.destroyAllWindows()
+
     # import the model
 
     # play the game
@@ -33,3 +57,7 @@ def main(args):
     # display the winner
 
     sys.exit()
+
+
+if __name__ == '__main__':
+    main(sys.argv)
